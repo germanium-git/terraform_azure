b5698393e7e4fbc68c360f004984c5bdc09eb95d
germanium-git@gmx.com <BzBKX35wL%AzFhP5b54BLd2$>
2020-01-07 09:17:59 +0100

    module vnet updated

--- 01-two_vm_one_vnet/main.tf
+++ 01-two_vm_one_vnet/main.tf
@@ -1,11 +1,22 @@
+module "myrg" {
+    source = "../modules/res_group"
+    subscription_id = var.subscription_id
+    client_id = var.client_id
+    client_secret = var.client_secret
+    tenant_id = var.tenant_id
+    rg_name = var.rg_name
+}
+
 module "vnet" {
     source = "../modules/vnet"
     subscription_id = var.subscription_id
     client_id = var.client_id
     client_secret = var.client_secret
     tenant_id = var.tenant_id
+    vnet_name = "MyTestVNet"
     vnet_address_space = "10.0.0.0/16"
-    rg_name = var.rg_name
+    dependency_rg_created = module.myrg.rg_created
+    rg_name = module.myrg.rs_group_name
 }
 
 module "subnet-01" {
@@ -15,6 +26,7 @@ module "subnet-01" {
     client_secret = var.client_secret
     tenant_id = var.tenant_id
     vnet_name = module.vnet.vnet_name
+    dependency_rg_created = module.myrg.rg_created
     rg_name = var.rg_name
     subnet_name = "internal-01"
     subnet_cidr = "10.0.1.0/24"
@@ -27,6 +39,7 @@ module "subnet-02" {
     client_secret = var.client_secret
     tenant_id = var.tenant_id
     vnet_name = module.vnet.vnet_name
+    dependency_rg_created = module.myrg.rg_created
     rg_name = var.rg_name
     subnet_name = "internal-02"
     subnet_cidr = "10.0.2.0/24"
@@ -39,7 +52,7 @@ module "ubuntu_vm-01" {
     client_secret         = var.client_secret
     tenant_id             = var.tenant_id
     vm_name               = "ubuntu-vm-01"
-    dependency_rg_created = module.vnet.rg_created
+    dependency_rg_created = module.myrg.rg_created
     rg_name               = var.rg_name
     nsg_id                = module.vnet.nsg_id
     subnet_id             = module.subnet-01.subnet-id
@@ -53,9 +66,10 @@ module "ubuntu_vm-02" {
     client_secret = var.client_secret
     tenant_id = var.tenant_id
     vm_name = "ubuntu-vm-02"
-    dependency_rg_created = module.vnet.rg_created
+    dependency_rg_created = module.myrg.rg_created
     rg_name = var.rg_name
     nsg_id = module.vnet.nsg_id
     subnet_id = module.subnet-02.subnet-id
     pubkey = var.pubkey
 }
+
