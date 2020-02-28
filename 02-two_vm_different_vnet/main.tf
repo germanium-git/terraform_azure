d39a9102685a4e89be661347745a79efb746240e
germanium-git@gmx.com <BzBKX35wL%AzFhP5b54BLd2$>
2020-02-28 11:38:10 +0100

    modules updated

--- 02-two_vm_different_vnet/main.tf
+++ 02-two_vm_different_vnet/main.tf
@@ -1,100 +1,110 @@
 module "myrg" {
-    source = "../modules/res_group"
-    subscription_id = var.subscription_id
-    client_id = var.client_id
-    client_secret = var.client_secret
-    tenant_id = var.tenant_id
-    rg_name = var.rg_name
+    source                  = "../modules/res_group"
+    subscription_id         = var.subscription_id
+    client_id               = var.client_id
+    client_secret           = var.client_secret
+    tenant_id               = var.tenant_id
+    rg_name                 = var.rg_name
 }
 
 module "vnet-01" {
-    source = "../modules/vnet"
-    subscription_id = var.subscription_id
-    client_id = var.client_id
-    client_secret = var.client_secret
-    tenant_id = var.tenant_id
-    vnet_name = "vnet-01"
-    vnet_address_space = "10.1.0.0/16"
-    dependency_rg_created = module.myrg.rg_created
-    rg_name = var.rg_name
+    source                  = "../modules/vnet"
+    subscription_id         = var.subscription_id
+    client_id               = var.client_id
+    client_secret           = var.client_secret
+    tenant_id               = var.tenant_id
+    vnet_name               = "vnet-01"
+    vnet_address_space      = var.vnet01_address_space
+    dependency_rg_created   = module.myrg.rg_created
+    rg_name                 = var.rg_name
+    nsg_name                = "IntraVNET-01"
+    nsg_rules               = var.nsg_rules
 }
 
 module "vnet-02" {
-    source = "../modules/vnet"
-    subscription_id = var.subscription_id
-    client_id = var.client_id
-    client_secret = var.client_secret
-    tenant_id = var.tenant_id
-    vnet_name = "vnet-02"
-    vnet_address_space = "10.2.0.0/16"
-    dependency_rg_created = module.myrg.rg_created
-    rg_name = var.rg_name
+    source                  = "../modules/vnet"
+    subscription_id         = var.subscription_id
+    client_id               = var.client_id
+    client_secret           = var.client_secret
+    tenant_id               = var.tenant_id
+    vnet_name               = "vnet-02"
+    vnet_address_space      = var.vnet02_address_space
+    dependency_rg_created   = module.myrg.rg_created
+    rg_name                 = var.rg_name
+    nsg_name                = "IntraVNET-02"
+    nsg_rules               = var.nsg_rules
 }
 
 module "subnet-01" {
-    source = "../modules/subnet"
-    subscription_id = var.subscription_id
-    client_id = var.client_id
-    client_secret = var.client_secret
-    tenant_id = var.tenant_id
-    vnet_name = module.vnet-01.vnet_name
-    dependency_rg_created = module.myrg.rg_created
-    rg_name = var.rg_name
-    subnet_name = "internal-01"
-    subnet_cidr = "10.1.0.0/24"
+    source                  = "../modules/subnet"
+    subscription_id         = var.subscription_id
+    client_id               = var.client_id
+    client_secret           = var.client_secret
+    tenant_id               = var.tenant_id
+    vnet_name               = module.vnet-01.vnet_name
+    dependency_rg_created   = module.myrg.rg_created
+    dependency_vnet_created = module.vnet-01.vnet_created
+    rg_name                 = var.rg_name
+    subnet_name             = "internal-01"
+    subnet_cidr             = cidrsubnet(var.vnet01_address_space, 8, 0)
 }
 
 module "subnet-02" {
-    source = "../modules/subnet"
-    subscription_id = var.subscription_id
-    client_id = var.client_id
-    client_secret = var.client_secret
-    tenant_id = var.tenant_id
-    vnet_name = module.vnet-02.vnet_name
-    dependency_rg_created = module.myrg.rg_created
-    rg_name = var.rg_name
-    subnet_name = "internal-02"
-    subnet_cidr = "10.2.0.0/24"
+    source                  = "../modules/subnet"
+    subscription_id         = var.subscription_id
+    client_id               = var.client_id
+    client_secret           = var.client_secret
+    tenant_id               = var.tenant_id
+    vnet_name               = module.vnet-02.vnet_name
+    dependency_rg_created   = module.myrg.rg_created
+    dependency_vnet_created = module.vnet-02.vnet_created
+    rg_name                 = var.rg_name
+    subnet_name             = "internal-02"
+    subnet_cidr             = cidrsubnet(var.vnet02_address_space, 8, 0)
 }
 
 module "ubuntu_vm-01" {
-    source                = "../modules/ubuntu_vm"
-    subscription_id       = var.subscription_id
-    client_id             = var.client_id
-    client_secret         = var.client_secret
-    tenant_id             = var.tenant_id
-    vm_name               = "ubuntu-vm-01"
-    dependency_rg_created = module.myrg.rg_created
-    rg_name               = var.rg_name
-    nsg_id                = module.vnet-01.nsg_id
-    subnet_id             = module.subnet-01.subnet-id
-    pubkey                = var.pubkey
+    source                  = "../modules/ubuntu_vm"
+    subscription_id         = var.subscription_id
+    client_id               = var.client_id
+    client_secret           = var.client_secret
+    tenant_id               = var.tenant_id
+    vm_name                 = "ubuntu-vm-01"
+    dependency_rg_created   = module.myrg.rg_created
+    rg_name                 = var.rg_name
+    nsg_id                  = module.vnet-01.nsg_id
+    subnet_id               = module.subnet-01.subnet-id
+    pubkey                  = file("../key/id_rsa.pub")
+    keypath                 = var.keypath
+    admin_username          = var.admin_username
 }
 
 module "ubuntu_vm-02" {
-    source = "../modules/ubuntu_vm"
-    subscription_id = var.subscription_id
-    client_id = var.client_id
-    client_secret = var.client_secret
-    tenant_id = var.tenant_id
-    vm_name = "ubuntu-vm-02"
-    dependency_rg_created = module.myrg.rg_created
-    rg_name = var.rg_name
-    nsg_id = module.vnet-02.nsg_id
-    subnet_id = module.subnet-02.subnet-id
-    pubkey = var.pubkey
+    source                  = "../modules/ubuntu_vm"
+    subscription_id         = var.subscription_id
+    client_id               = var.client_id
+    client_secret           = var.client_secret
+    tenant_id               = var.tenant_id
+    vm_name                 = "ubuntu-vm-02"
+    dependency_rg_created   = module.myrg.rg_created
+    rg_name                 = var.rg_name
+    nsg_id                  = module.vnet-02.nsg_id
+    subnet_id               = module.subnet-02.subnet-id
+    pubkey                  = file("../key/id_rsa.pub")
+    keypath                 = var.keypath
+    admin_username          = var.admin_username
 }
 
 module "peering_vnet-01-to-02" {
-    source = "../modules/vnet_peering"
-    subscription_id = var.subscription_id
-    client_id = var.client_id
-    client_secret = var.client_secret
-    tenant_id = var.tenant_id
-    dependency_rg_created = module.myrg.rg_created
-    rg_name = var.rg_name
-    source_vnet_name = module.vnet-01.vnet_name
-    source_vnet_id = module.vnet-01.vnet_id
-    destination_vnet_name = module.vnet-02.vnet_name
-    destination_vnet_id = module.vnet-02.vnet_id
+    source                  = "../modules/vnet_peering"
+    subscription_id         = var.subscription_id
+    client_id               = var.client_id
+    client_secret           = var.client_secret
+    tenant_id               = var.tenant_id
+    dependency_rg_created   = module.myrg.rg_created
+    rg_name                 = var.rg_name
+    source_vnet_name        = module.vnet-01.vnet_name
+    source_vnet_id          = module.vnet-01.vnet_id
+    destination_vnet_name   = module.vnet-02.vnet_name
+    destination_vnet_id     = module.vnet-02.vnet_id
 }
--- 02-two_vm_different_vnet/variables.tf
+++ 02-two_vm_different_vnet/variables.tf
@@ -7,6 +7,43 @@ variable "rg_name" {
   default = "rg-zsto-networking-nemedpet"
 }
 
-variable "pubkey" {
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDKRP0OEAWmAxOTtUZ9beQmKm8t0sgghVAFj1QaseDbMFSprtznHXcU/EF7YWR4wgLI25xzj8aItXxbkNav0mSoHnsyBAmDi42XUCQ5j3snGmF8f8XXu0j49d8z5cBgmiRCheoIC31EtYGzu5bkiuhOOj0k5hAOVzMVgKRdcMasRRsnAQ+VRHtR1dAYRKYQQzaKUtbdfi42xgj4QlPWM0g1t8gOA/Z52hVdX564Zh5GatEQ5QFNTuA7OI4DH1B5E5zZJLNSwE+2Kyf2VZdwc/piIX3X5s1PnJ672JZOoVKwDEMgNYWXycosZBKt4hvbyC5sSiZYQbcBJ5REv9yJRNLwZ7r/Isr5kBYebZBsfOxa36XgKKvjvNGfL5JXNYcCA6nYoaqN+W4UMNjOQg37fDaUVNUxkNFJoUUaaOihYhrWr+Ak79TB7J3ej7huAyvYCZ+bjQOTLgcBinWBsHFTzrzyBtyKTxZRhwEMCpFXljwwtRWN9M7I21XcIvlcsUwNFEzFwmyYNiTg7PKFIwu+KttQXCeVf+WnAEovJe2lGpgsJTrngyeR8M1Pfe92NXe7o2CZ9hroxmULuXahWHro99/InZkobQOIBKEJ7jnppzomXapjpKtEDudQfhMLd8PXS9nnt8xv0Ldpoz9je1l37hRr6T2Ei5RLCM5cRtT2HWX/SQ== Ansible Tower"
+variable "admin_username" {
+  default = "azureuser"
+}
+
+# The path of the destination file on the virtual machine where the key will be written.
+variable "keypath" {
+  default = "/home/azureuser/.ssh/authorized_keys"
+}
+
+
+variable "vnet_name" {
+  default = "MyTestVNet"
+}
+
+variable "vnet01_address_space" {
+ default = "10.1.0.0/16"
+}
+
+variable "vnet02_address_space" {
+ default = "10.2.0.0/16"
+}
+
+
+# Specify rules in the Network Security Group protecting the management access to FortiGate Firewall
+variable "nsg_rules" {
+  description = "Network Security Group"
+  type = map(list(string))
+
+  # Structure is as follows name = [priority, direction, access, protocol, source_port_range, destination_port_range, source_address_prefix, destination_address_prefix]
+  default = {
+    #allowall = ["100", "Inbound", "Allow", "*", "*", "*", "*", "*",]
+    SSH   = ["110", "Inbound", "Allow", "Tcp", "*", "22", "193.15.240.60/32", "*",]
+    AllVNETIn = ["100", "Inbound", "Allow", "*", "*", "*", "10.0.0.0/8", "*",]
+    OutAll = ["100", "Outbound", "Allow", "*", "*", "*", "*", "*",]
+  }
+}
+
+variable "mng_access_from" {
+  default = "193.15.240.60/32"
 }
